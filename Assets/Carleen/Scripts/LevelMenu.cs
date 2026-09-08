63ded72da1ee9b1340b82d7e31eed7aa42b7a68b
c4rt4m <cjtam18@gmail.com>
2026-09-08 13:19:44 +1200

    Unlocking Next Levels
    Completion button loads Level Scene and unlocks next level

--- Assets/Carleen/Scripts/LevelMenu.cs
+++ Assets/Carleen/Scripts/LevelMenu.cs
@@ -4,16 +4,21 @@ using UnityEngine.UI;
 
 public class LevelMenu : MonoBehaviour
 {
+    
     public Button[] buttons;
+
+    public static int currentLevel;
 
     private void Awake()
     {
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
-        for(int i = 0; i < buttons.Length; i++)
+
+        for (int i = 0; i < buttons.Length; i++)
         {
             buttons[i].interactable = false;
         }
-        for(int i = 0; i < unlockedLevel; i++)
+
+        for (int i = 0; i < unlockedLevel; i++)
         {
             buttons[i].interactable = true;
         }
@@ -21,6 +26,8 @@ public class LevelMenu : MonoBehaviour
 
     public void OpenLevel(int levelId)
     {
+        currentLevel = levelId;
+
         string levelName = "Level " + levelId;
         SceneManager.LoadScene(levelName);
     }
